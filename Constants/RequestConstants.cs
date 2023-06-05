1da5bf30728a4df596a3f99dec069ba8f380295f
Karam Ajaj <karam.ajaj@hotmail.com>
2023-06-05 15:37:26 +0200

    project files added

---
+++ Constants/RequestConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace RESTfulAPIConsume.Constants
+{
+    
+    public static class RequestConstants {
+
+        public const string APIUrl = "http://Veeam.FQDN/api";
+        public const string SessionRequestUrl = APIUrl + "/sessionMngr/";
+        public const string GetReportsUrl = APIUrl + "/backupFiles?format=Entity";
+        public const string path_json = @"D:\folder\downloads\report.json";
+        public const string path_csv = @"D:\folder\downloads\report.csv";
+        public const string path_xlsx = @"D:\folder\downloads\report.xlsx";
+
+        public const string usernName = "administrator";
+        public const string password = "password";
+        // authorization header format is "username:password"
+        public const string auth = usernName + ":" + password;
+        public const string UserAgent = "User-Agent";
+        public const string UserAgentValue = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using RESTfulAPIConsume.Constants;
+using RESTfulAPIConsume.RequestHandlers;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Data;
+using Aspose.Cells;
+using Aspose.Cells.Utility;
+using ChoETL;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+
+namespace RESTfulAPIConsume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IRequestHandler httpWebRequestHandler = new HttpWebRequestHandler();
+            
+            //post request
+            var postRequest = (HttpWebRequest)WebRequest.Create(RequestConstants.SessionRequestUrl);
+            var postData = "" + Uri.EscapeDataString("");
+            var data = Encoding.ASCII.GetBytes(postData);
+            postRequest.Method = "POST";
+            postRequest.ContentType = "application/x-www-form-urlencoded";
+            postRequest.ContentLength = 0;
+            postRequest.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.Default.GetBytes(RequestConstants.auth));
+            //Console.WriteLine(RequestConstants.auth);
+            using (var stream = postRequest.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+            var response = (HttpWebResponse)postRequest.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            // get request:
+            // define the download URL and download the JSON file to local storage
+            WebRequest getRequest = WebRequest.Create(RequestConstants.GetReportsUrl);
+            getRequest.Method = "GET";
+            getRequest.Headers["Authorization"] = "Basic " + Convert.ToBase64String(Encoding.Default.GetBytes(RequestConstants.auth));
+            // error post and get should be in the same session to get the data back
+            HttpWebResponse getResponse = getRequest.GetResponse() as HttpWebResponse;
+            // display response on console
+            //documentation: https://docs.microsoft.com/en-us/dotnet/api/system.net.webrequest?view=net-6.0
+            // Get the stream containing content returned by the server.
+            Stream dataStream = getResponse.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            //Console.WriteLine(responseFromServer);
+            // Cleanup the streams and the response.
+            reader.Close();
+            dataStream.Close();
+            getResponse.Close();
+            
+            // write JSON reponse to local file
+            //File.WriteAllText(RequestConstants.path_json, responseFromServer);
+            
+
+            Console.WriteLine(getResponse);
+            Console.ReadLine();
+            Console.WriteLine("done");
+        }
+
+        public static string GetId(IRequestHandler requestHandler)
+        {
+            return requestHandler.GetId(RequestConstants.APIUrl);
+        }
+
+        
+        
+    }
+}
